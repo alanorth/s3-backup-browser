df4870803bc7c364fcb6806572e5c1406264adb6
Alan Orth <alan.orth@gmail.com>
2011-01-11 09:11:25 +0300

    Parse the S3 XML and put all backups into structured arrays 	daily	-> dbname	->	backup1 	daily	-> dbname	->	backup2 	weekly	-> dbname	->	backup1 	weekly	-> dbname	->	backup2

--- index.php
+++ index.php
@@ -10,16 +10,52 @@
     $bucketXml = file_get_contents($bucketUrl);
     $xmlObj = simplexml_load_string($bucketXml);
 
-#   echo "<pre>\n";
-#   print_r($xmlObj);
-#   echo "</pre>\n";
+	$daily = array();
+	$weekly = array();
+	$monthly = array();
 
     foreach ( $xmlObj->Contents as $content ) { 
-        echo 'Name: ' . $content->Key . "<br />\n";
-        echo 'Size: ' . $content->Size . "<br />\n";
-        echo "<br />\n";
+#        echo 'Name: ' . $content->Key . "<br />\n";
+#        echo 'Size: ' . $content->Size . "<br />\n";
+#        echo "<br />\n";
+		# check if the current file ("Key" in Amazon's S3 XML) is daily, weekly, etc
+		# example: //mysql/monthly/azizi/azizi_2010-09-01_03h15m.September.azizi.sql.gz
+		if(preg_match('/^mysql\/daily/i',$content->Key)) {
+			# grab the dbname from the Key
+			list(,,$dbname,$filename) = explode('/',$content->Key); // is there a better way to extract
+															// only one or two elements from an array?
+															// like in perl's array("1","2")[0]?
+			$size = (string) $content->Size; # cast Size to a string, otherwise it's still a SimpleXML object
+#			$lastmodified = $content->LastModified;
+			if(!array_key_exists("$dbname",$daily)) {
+				#array_push($daily,"$dbname"=>array());
+				$daily["$dbname"] = array();
+			}
+			array_push($daily["$dbname"],array('size'=>$size));
+		}
+		else if(preg_match('/^mysql\/weekly/i',$content->Key)) {
+			list(,,$dbname,) = explode('/',$content->Key);
+			if(!array_key_exists("$dbname",$weekly)) {
+				$weekly["$dbname"] = array();
+			}
+			array_push($weekly["$dbname"],array('size'=>$size));
+		}
+		else if(preg_match('/^mysql\/monthly/i',$content->Key)) {
+			list(,,$dbname,) = explode('/',$content->Key);
+			if(!array_key_exists("$dbname",$monthly)) {
+				$monthly["$dbname"] = array();
+			}
+			array_push($monthly["$dbname"],array('size'=>$size));
+		}
     }   
     
+	echo "<pre>";
+	print_r($daily);
+	echo "</pre>";
+	echo "<pre><h2>weekly</h2>";
+	print_r($weekly);
+	echo "</pre>";
+
     echo count($xmlObj->Contents);
 ?>
 </body>
